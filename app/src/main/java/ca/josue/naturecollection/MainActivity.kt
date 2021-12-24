072de1d97051199441e8ed4b582fe3d766cef3c3
josue-lubaki <70981701+josue-lubaki@users.noreply.github.com>
2021-12-24 00:58:29 -0500

    initial launch

--- app/src/main/java/ca/josue/naturecollection/MainActivity.kt
+++ app/src/main/java/ca/josue/naturecollection/MainActivity.kt
@@ -2,10 +2,17 @@ package ca.josue.naturecollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ca.josue.naturecollection.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // injecter le fragment dans notre boite
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, HomeFragment())
+        transaction.addToBackStack(null) // annuler le retour vers ce composant
+        transaction.commit()
     }
 }
